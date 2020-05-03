d784239a9c85ede767cc6e81ba86278bfe7d94c9
Vaibhav <vaibhav@bluesapling.com>
2020-05-03 10:44:18 +0530

    Finish exercise 1.11
    - Display stats in a table

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,13 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Button from './feedback/Button';
 
+const Statistic = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all ? ((good - bad) / all).toFixed(1) : 'NA';
+  const positive = all ? `${((good * 100) / all).toFixed(1)} %` : 'NA';
+
+  if (!all) return <p>No feedback given</p>
+
+  return (
+    <>
+      <h1>statistics</h1>
+
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={all} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positive" value={positive} />
+        </tbody>
+      </table>
+    </>
+  );
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad;
 
   return (
     <>
@@ -19,14 +51,7 @@ const App = () => {
       <Button name="nuetral" handleClick={() => setNeutral(neutral + 1)} />
       <Button name="bad" handleClick={() => setBad(bad + 1)} />
 
-      <h1>statistics</h1>
-
-      <p>{`good ${good}`}</p>
-      <p>{`nuetral ${neutral}`}</p>
-      <p>{`bad ${bad}`}</p>
-      <p>{`all ${good + neutral + bad}`}</p>
-      <p>{`average ${(good - bad) / total}`}</p>
-      <p>{`positive ${(good * 100) / total}`}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
