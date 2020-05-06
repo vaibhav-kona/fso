2d3ae7054dcb0c4a13a69f5d3499e9d394e55dbd
Vaibhav <vaibhav@bluesapling.com>
2020-05-06 15:05:41 +0530

    Finish exercises 2.1 and 2.2

---
+++ part2/courseContents/src/Course.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => parts.map((part) => <Part key={part.name} part={part} />);
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Footer = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p><b>total of {total} exercises</b></p>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+
+      <Content parts={course.parts} />
+
+      <Footer parts={course.parts} />
+    </>
+  );
+}
+
+export default Course;
