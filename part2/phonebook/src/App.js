ae027761f395180c559e4bc4db93dc56a72e0b5f
Vaibhav <vaibhav@bluesapling.com>
2020-05-09 10:45:39 +0530

    Finish exercise 2.11
    - Use axios API to fetch persons list from API

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const FilterForm = ({ filter, handleFilterChange }) => (
   <>
@@ -45,15 +46,17 @@ const Persons = ({ persons, filter }) => (
 const Person = ({ person }) => <p>{person.name} {person.number}</p>;
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data);
+      });
+  }, []);
 
   const handleNameChange = (e) => {
     e.preventDefault();
