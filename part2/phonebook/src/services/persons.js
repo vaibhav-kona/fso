d04cd1240710c648479e1e31f49dcf07c3ad39dd
Vaibhav <vaibhav@bluesapling.com>
2020-09-18 19:06:04 +0530

    Update API endpoints

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
 const getAll = () => {
-  const request = axios.get('http://localhost:3001/persons');
+  const request = axios.get('http://localhost:3002/api/persons');
   return request.then((response) => response.data);
 }
 
 const create = (newPerson) => {
-  const request = axios.post('http://localhost:3001/persons', newPerson);
+  const request = axios.post('http://localhost:3002/api/persons', newPerson);
   return request.then((response) => response.data);
 }
 
 const update = (personId, personData) => {
-  const request = axios.put(`http://localhost:3001/persons/${personId}`, personData);
+  const request = axios.put(`http://localhost:3002/api/persons/${personId}`, personData);
   return request.then((response) => response.data);
 }
 
 const remove = (personId) => {
-  const request = axios.delete(`http://localhost:3001/persons/${personId}`);
+  const request = axios.delete(`http://localhost:3002/api/persons/${personId}`);
   return request.then((response) => response.data);
 }
 
